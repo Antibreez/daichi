00ddf77ea8b84628c68e6f0c21ff0730cbb3e645
malibu911 <arkadev.sx@gmail.com>
2021-01-04 18:57:36 +0300

    done for all rs

---
+++ app/js/service-modal-slider.js
@@ -0,0 +1,26 @@
+if (window.innerWidth < 768) {
+  $(".service-popup-container").slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    arrows: false,
+    variableWidth: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: ".service-popup-mobile-navigation",
+    centerMode: true,
+  });
+
+  $(".service-popup-mobile-navigation").slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    arrows: false,
+    variableWidth: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: ".service-popup-container",
+    centerMode: true,
+    focusOnSelect: true,
+  });
+}
