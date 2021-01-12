1371fe4b266971278f8fc19e3b954afd459e9e2b
unknown <romanyuzyukfront@gmail.com>
2021-01-13 00:51:35 +0300

    fixses dashboard

--- app/js/dshboard-sliders.js
+++ app/js/dshboard-sliders.js
@@ -1,30 +1,40 @@
 // слайдер устройств
 
 if (window.innerWidth < 1200) {
-  $(".product-item").unwrap();
+    $(".product-item").unwrap();
 }
 
 $(".products-box-slider-js").slick({
-  dots: false,
-  infinite: false,
-  speed: 300,
-  prevArrow: ".dvc-prev-btn",
-  nextArrow: ".dvc-next-btn",
-  variableWidth: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
+    dots: false,
+    infinite: false,
+    speed: 300,
+    prevArrow: ".dvc-prev-btn",
+    nextArrow: ".dvc-next-btn",
+    // variableWidth: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
 
-  responsive: [
-    {
-      breakpoint: 1200,
-      settings: {
-        arrows: false,
-      },
-    },
-  ],
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                arrows: false,
+            },
+        },
+        {
+            breakpoint: 769,
+            settings: {
+                slidesToShow: 3,
+            },
+        },
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 2,
+            },
+        },
+    ],
 });
-
-
 
 
 // $(".products-box-slider-js").filter('.slick-initialized').slick("unslick");
@@ -38,34 +48,60 @@ $(".products-box-slider-js").slick({
 
 //слайдер активных подписок
 $(".db_subscribe-cards-box").slick({
-  dots: false,
-  infinite: false,
-  speed: 300,
-  variableWidth: true,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  prevArrow: ".sbr-prev-btn",
-  nextArrow: ".sbr-next-btn",
-
-  responsive: [
-    {
-      breakpoint: 1200,
-      settings: {
-        arrows: false,
-      },
-    },
-  ],
+    dots: false,
+    infinite: false,
+    speed: 300,
+    // variableWidth: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: ".sbr-prev-btn",
+    nextArrow: ".sbr-next-btn",
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                arrows: false,
+            },
+        },
+        {
+            breakpoint: 769,
+            settings: {
+                slidesToShow: 3,
+            },
+        },
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 2,
+                variableWidth: true,
+            },
+        },
+    ],
 });
 
 // слайдер промо-карточек подписок
 if (window.innerWidth < 768) {
-  $(".sbr-promo-box_db").slick({
-    dots: false,
-    infinite: false,
-    speed: 300,
-    arrows: false,
-    variableWidth: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  });
+    $(".sbr-promo-box_db").slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        arrows: false,
+        // variableWidth: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    variableWidth: true,
+                },
+            },
+        ],
+    });
 }
